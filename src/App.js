cad0e9f8a804781e4132ee51442539721230afa6
zgabros <zgabros@gmail.com>
2022-07-04 14:58:46 -0300

    nueva base

--- src/App.js
+++ src/App.js
@@ -11,16 +11,16 @@ import FormularioProcedimientos from "./componentes/FormularioProcedimientos/For
 
 function App() {
   const firebaseConfig = {
-    apiKey: "AIzaSyCoKPOo0r9jjHkflNDHuSIpBfIQ6xSj7Ks",
-    authDomain: "saq-app-a81e1.firebaseapp.com",
-    projectId: "saq-app-a81e1",
-    storageBucket: "saq-app-a81e1.appspot.com",
-    messagingSenderId: "397246895422",
-    appId: "1:397246895422:web:1e2b5ccdc5b8cc914c886f",
+    apiKey: "AIzaSyBqIq4scg5qMcb3zfd9Oz3CV3ILV0kgr3M",
+    authDomain: "facturizer.firebaseapp.com",
+    projectId: "facturizer",
+    storageBucket: "facturizer.appspot.com",
+    messagingSenderId: "658762463793",
+    appId: "1:658762463793:web:17118b10fa0076c2213310",
+    measurementId: "G-KV6GJTRGGL",
   };
-
-  // Initialize Firebase
-  initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
   return (
     <div className="App">
